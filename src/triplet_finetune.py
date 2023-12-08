733bc8681fd00c079feb681852b1cf2b68cdab3c
ShulzhenkoPetr <petro.shulzhenko@gmail.com>
2023-12-08 12:59:50 +0100

    added RFC

--- src/triplet_finetune.py
+++ src/triplet_finetune.py
@@ -20,16 +20,10 @@ from utils.metrics import accuracy_metric, f1_metric
 
 from sentence_transformers import SentenceTransformer, InputExample, losses
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from torch.utils.data import DataLoader
 from utils.custom_datasets import triplet_data, sentences_data
 
-
-
-#Define your train examples. You need more than just two examples...
-# train_examples = [InputExample(texts=['My first sentence', 'My second sentence'], label=0.8),
-#     InputExample(texts=['Another pair', 'Unrelated sentence'], label=0.3)]
-#
-# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 train_data = triplet_data(data_folder='dataset/all_training_data/train/',
@@ -50,6 +44,9 @@ enc_train_sentences = model.encode(train_sentences, show_progress_bar=True)
 clf = DecisionTreeClassifier(random_state=0)
 clf.fit(enc_train_sentences, train_labels)
 
+rf = RandomForestClassifier(random_state=0)
+rf.fit(enc_train_sentences, train_labels)
+
 
 val_sentences, val_labels = sentences_data(data_folder='dataset/all_training_data/val/',
                                            label_path='dataset/all_training_data/val_labels.json',
@@ -57,14 +54,20 @@ val_sentences, val_labels = sentences_data(data_folder='dataset/all_training_dat
 
 enc_val_sentences = model.encode(val_sentences, show_progress_bar=True)
 val_predictions = clf.predict(enc_val_sentences)
+rf_preds = rf.predict(enc_val_sentences)
 
 accuracy = accuracy_metric(val_predictions, val_labels)
 f1_score = f1_metric(val_predictions, val_labels)
+
+rf_acc = accuracy_metric(rf_preds, val_labels)
+rf_f1 = f1_metric(rf_preds, val_labels)
 
 print(val_predictions)
 
 print("Val accuracy", accuracy)
 print("Val f1 score", f1_score)
+print("RF val accuracy", rf_acc)
+print("RF val f1 score", rf_f1)
 
 log_results('logs/sbert_finetune.txt',
             'finetuned sbert model with DecisionTree',
